c590e7a795756d3f9a9f875f946f56b2029af452
Shuaib Salam <shuaibsalam1234@gmail.com>
2024-02-21 12:17:45 +0530

    final commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './Navbar.scss';
-
+import { RiMenu5Fill } from "react-icons/ri";
 
 const Navbar = () => {
 
     const tab = [ 'Courses', 'Events', 'Skill Parks', 'About', 'Contact'];
+
+    const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+    const handleMenuOpen=()=>{
+        setIsMenuOpened(!isMenuOpened);
+    }
 
   return (
     <nav>
@@ -18,6 +25,16 @@ const Navbar = () => {
             <button className='login'>Login</button>
             <button className='getStarted'>Get Started</button>
         </div>
+        <RiMenu5Fill className='menuIcon'onClick={handleMenuOpen}/>
+        { isMenuOpened && 
+          <div className="menu">
+            {
+                tab.map((text,i)=>(
+                    <h4 key={i}>{text}</h4>
+                ))
+            }
+          </div>
+        }
     </nav>
   )
 }
