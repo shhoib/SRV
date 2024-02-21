bfe6c260c152d340c39c7c40212dc253df6183bf
Shuaib Salam <shuaibsalam1234@gmail.com>
2024-02-21 11:30:56 +0530

    final commit

--- src/Pages/LandinPage/LandingPage.jsx
+++ src/Pages/LandinPage/LandingPage.jsx
@@ -7,7 +7,7 @@ import Footer from "../../components/footer/Footer"
 const LandingPage = () => {
   return (
     <>
-    {/* <Navbar/> */}
+    <Navbar/>
     <Banner/>
     <Home/>
     <Card/>
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import './Banner.scss';
-import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowForward ,IoMdClose} from "react-icons/io";
 
 
 const Banner = () => {
@@ -11,6 +11,12 @@ const Banner = () => {
         </div>
         <img src='./IMG/robo.jpg' alt="" className='bannerIMG'/>
         <div className='bannerBG'></div>
+        <div className="close1"><IoMdClose/></div>
+        <div className="rectangle1"></div>
+        <div className="rectangle2"></div>
+        <div className="circle"></div>
+        <div className="close2"><IoMdClose/></div>
+        <div className="close3"><IoMdClose/></div>
     </section>
   )
 }
